a5df7c38a17a2771c0d63ece2693164139c83b85
Mesh <mesh@kali.kali>
2020-03-28 14:52:46 -0400

    added registration view

---
+++ myBlog/blog/views.py
@@ -0,0 +1,66 @@
+from django.shortcuts import render,redirect
+from .forms import AddBlogForm,UserForm,UserProfileInfoForm
+from .models import Post
+import datetime,timeago,timedelta
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse,HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth import authenticate,login,logout
+# Create your views here.
+
+
+def index(request):
+
+    content = Post.objects.all().filter(status='published').order_by('-created')
+    context = {'content':content}
+    return render(request,'index.html',context)
+
+@login_required
+def add_blog(request):
+    form = AddBlogForm()
+    
+    if request.method == "POST":
+        form = AddBlogForm(request.POST)
+        if form.is_valid():
+            form.save()
+            form = AddBlogForm()
+            return redirect('index')
+    context = {'form':AddBlogForm}
+    return render(request,'add_blog.html',context)
+
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('index'))
+
+def register(request):
+    registered = False
+    if request.method == 'POST':
+        user_form = UserForm(data=request.POST)
+        profileForm = UserProfileInfoForm(data = request.POST)
+
+        if user_form.is_valid and profileForm.is_valid():
+            user = user_form.save()
+            user.set_password(user.password)
+            user.save()
+            profile = profileForm.save(commit=False)
+            profile.user = user
+            if 'profile_pic' in request.FILES['profile_pic']:
+                print('profile pic present')
+                profile.profile_pic = request.FILES['profile_pic']
+
+            profile.Save()
+            registered = True
+        
+        else:
+            print(UserForm.errors,profileForm.errors)
+
+    else:
+        user_form = UserForm()
+        profileForm = UserProfileInfoForm()
+
+    context = {
+        'user_form':UserForm,
+        'profileForm':UserProfileInfoForm,
+        'registered':registered
+    }
+    return render(request,'registration.html',context)
